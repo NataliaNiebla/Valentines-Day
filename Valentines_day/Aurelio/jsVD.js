645d6cb2088f37381d9de58047e4b35075f80f83
NataliaNiebla <tataniebla2005@gmail.com>
2025-02-13 23:49:09 -0600

    1234567

---
+++ Valentines_day/Aurelio/jsVD.js
@@ -0,0 +1,3 @@
+function abrirSobre() { // Función para abrir el sobre
+    document.querySelector('.container').classList.toggle('abierto'); // Selecciona el primer elemento que coincida con el selector y le añade o quita la clase 'abierto'
+}
